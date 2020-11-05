4e50bea166621e824e738636714d2881239a0764
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-11-05 16:34:57 +0800

    Update CPE14.cpp

--- CPE14.cpp
+++ CPE14.cpp
@@ -3,19 +3,27 @@ using namespace std;
 
 int main()
 {
-   for(;;)
-   {
-      int year[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-      string week[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-      int month=0, day=0, sum=0 , date=0;
-      cin >> month>>day;
-      for( int i = 0; i < (month-1) ; i++ )
-      {
-         sum += year[ i ];   
-      }
-      date = ( sum + day +4 ) % 7;
-      cout << week[ date ];
-   }
+   
+        int year[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        string week[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        int month = 0, day = 0, sum = 0, date = 0;
+        int numcases;
+        while (cin >> numcases)
+        {
+            for (int i = 0; i < numcases; i++)
+            {
+                cin >> month >> day;
+                
+                date = 0;
+                sum = 0;
+                for (int i = 0; i < (month - 1); i++)
+                {
+                    sum += year[i];
+                }
+                date = (sum + day + 4) % 7;
+                cout << week[date]<<endl;         
+            }
+        }      
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #include <iostream>
